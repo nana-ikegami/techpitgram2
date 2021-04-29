232fd5f590aa95bd73ea1019ee3153b56bdc1a7a
nana-ikegami <80028527+nana-ikegami@users.noreply.github.com>
2021-04-29 11:07:32 +0900

    コントローラーの編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,10 @@ class PostsController < ApplicationController
     end
   end
 
+  def index
+    @posts = Post.limit(10).order('created_at DESC')
+  end
+
   private
     def post_params
       params.require(:post).permit(:caption, photos_attributes: [:image]).merge(user_id: current_user.id)
